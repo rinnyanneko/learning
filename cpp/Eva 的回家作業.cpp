14350d1dc99da1429504e141e71e77ba33945fa6
rinnyanneko <rinnyanneko@outlook.com>
2023-06-21 10:16:23 +0800

    umu

---
+++ cpp/Eva 的回家作業.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t=0;
+    int a=0, b=0, c=0, d=0;
+    int ans=0;
+    cin>>t;
+    for(int i=0, i<t, i++)
+    {
+        cin>>a>>b>>c>>d;
+        if (d-c==a-b)
+        {
+            ans=d+(d-c);
+            cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<ans+"\n";
+        }
+        else
+        {
+            ans=d*(d/c);
+            cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<ans+"\n";
+        }
+    }
+}
