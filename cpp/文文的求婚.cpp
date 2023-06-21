af640b139a90048eeab41b1dbba48a09de64c34c
rinnyanneko <rinnyanneko@outlook.com>
2023-06-21 10:09:19 +0800

    owo

--- cpp/文文的求婚.cpp
+++ cpp/文文的求婚.cpp
@@ -7,7 +7,7 @@ int main()
     int y=0;
     while (cin>>y)
     {
-        if (y % 4 == 0 and y % 100 != 0 and y % 400 != 0)
+        if (y % 4 == 0 and y % 100 != 0 or y % 400 == 0)
         {
             cout << "閏年\n";
         }
