b75cb763191604532b25b412f28e9eff2fc4c1a4
rinnyanneko <rinnyanneko@outlook.com>
2023-08-12 14:37:18 +0800

    ch0101 ch0201

---
+++ gjun/C++_程式語言/Example/Prog1/ch0101.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+//main function
+int main(){
+    cout << "Hello ";
+    cout << "owobb" << endl;
+    printf("I like coding\n");
+    printf("Good luck to you.\n");
+    system("pause");
+    return 0;
+}
---
+++ gjun/C++_程式語言/Example/Prog1/ch0201.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age=18;
+    short var1;
+    cout << "Please enter the age of student: ";
+    cin >> age;
+    cout << "age of student is " << age << " years old"<<endl;
+    age = 22;
+    cout << "age of student is " << age << " years old"<<endl;
+    system("pause");
+    return 0;
+}
