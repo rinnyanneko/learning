cbd48b847f0bfb0f7f851d2475d77191ce768de6
rinnyanneko <rinnyanneko@outlook.com>
2023-08-26 10:16:46 +0800

    tab

--- gjun/C++_程式語言/Example/ch04/calculate.cpp
+++ gjun/C++_程式語言/Example/ch04/calculate.cpp
@@ -9,21 +9,21 @@ int main()
     cin >>op;
     switch (op)  //取得運算子 
     {
-    case '+':   //執行加法運算 
-        cout << "a+b = " << a+b <<"\n";
-        break;
-    case '-':   //執行減法運算 
-        cout << "a-b = " << a-b <<"\n";
-        break;
-    case '*':   //執行乘法運算 
-        cout << "a*b = " << a*b <<"\n";
-        break;
-    case '/':   //執行除法運算 
-        cout << "a/b = " << a/b <<"\n";
-        break;
-    default:   //加減乘除以外的運算
-        cout << "無法執行運算！\n";
-        break;
+        case '+':   //執行加法運算 
+            cout << "a+b = " << a+b <<"\n";
+            break;
+        case '-':   //執行減法運算 
+            cout << "a-b = " << a-b <<"\n";
+            break;
+        case '*':   //執行乘法運算 
+            cout << "a*b = " << a*b <<"\n";
+            break;
+        case '/':   //執行除法運算 
+            cout << "a/b = " << a/b <<"\n";
+            break;
+        default:   //加減乘除以外的運算
+            cout << "無法執行運算！\n";
+            break;
     }
     system("pause");
     return 0;
