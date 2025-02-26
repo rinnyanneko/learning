e9ee8821bd0a13c4d9d5c9743349762d3588ad0a
凛喵 <rinnyanneko@outlook.com>
2025-02-26 11:00:05 +0800

    2025-02-26 class

---
+++ school/113-2/a.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(string args[]){
+    double a = 0, b = 0;
+    cout << "Please enter 1st number:";
+    cin >> a;
+    cout << "Please enter 2nd number:";
+    cin >> b;
+    cout <<"The answer is:" << a + b << endl;
+    system("PAUSE");
+    return 0;
+}
