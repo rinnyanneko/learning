b1bdd66ae3fe4c0f794459270efa2cb1c0ef086c
凛喵 <rinnyanneko@outlook.com>
2025-04-02 16:02:01 +0800

    learn rust

--- Rust/Hello Rust/main/src/main.rs
+++ Rust/Hello Rust/main/src/main.rs
@@ -1,4 +1,6 @@
 // https://kaochenlong.com/2023/09/30/option-and-result-in-rust.html
+// https://slint.dev/
+// https://rust-lang.tw/book-tw/ch04-01-what-is-ownership.html
 fn main() {
     println!("Hello Rust!");
 }
