8c6d1d9ba9a71e4b1e3f62450cf92f37ccbd4c52
凛喵 <rinnyanneko@outlook.com>
2025-05-07 16:01:39 +0800

    finish HW

--- school/113-2/cpp/20250507hw.cxx
+++ school/113-2/cpp/20250507hw.cxx
@@ -1,15 +1,18 @@
 #include <iostream>
 
 using namespace std;
+
 int arr[] = {5595, 1381, 12803, 8040, 10502, 22270, 26895, 25625, 9255};
+
 int main(){
-	for (int i = 0; i < sizeof(arr)-1; i++){
-		for (int j = 0; j < sizeof(arr)-2-i; j++){
+	int len = sizeof(arr) / sizeof(arr[0]);
+	for (int i = 0; i < len-1; i++){
+		for (int j = 0; j < len-2-i; j++){
 			if (arr[j] < arr[j+1])
-				swap(arr[i], arr[j]);
+				swap(arr[j], arr[j+1]);
 		}
 	}
-	for (int i = 0; i < sizeof(arr); i++)
+	for (int i = 0; i < len; i++)
 		cout << arr[i] << " ";
 	return 0;
 }
