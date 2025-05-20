18813320d8cd9539aff73bc1a2fc9fb4df2b7dc4
凛喵 <rinnyanneko@outlook.com>
2025-05-20 09:50:13 +0800

    ch8

--- Rust/Hello Rust/main/src/main.rs
+++ Rust/Hello Rust/main/src/main.rs
@@ -1,6 +1,6 @@
 // https://kaochenlong.com/2023/09/30/option-and-result-in-rust.html
 // https://slint.dev/
-// https://rust-lang.tw/book-tw/ch06-02-match.html
+// https://rust-lang.tw/book-tw/ch08-01-vectors.html
 // https://gist.github.com/weihanglo/3dc1af4b0c15cb9ec600f28a7b06ad2f
 fn main() {
     println!("Hello Rust!");
