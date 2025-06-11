6054933566b0a21d5f558921b8cd1a284e14a251
rinnyanneko <rinnyanneko@outlook.com>
2025-06-11 09:59:50 +0800

    bin tree

--- school/113-2/binary tree.cpp
+++ school/113-2/binary tree.cpp
@@ -2,30 +2,51 @@
 
 using namespace std;
 
-int a[10000];
+int A[10000];
+int d;
 
-void build_tree(int data, int index);
+void bt(int data, int nodeno);
+bool tt(int search_target, int nodeno);
 
 int main(){
-	for (int i = 0; i < 12; i++){
-		int d;
-		cin >> d;
-		build_tree(d, 0);
-	}
-	for (int i = 0; i < 24; i++){
-		cout << a[i] << " ";
-	}
+    for (int i = 1; i <= 10; i++){
+        cin >> d;
+        bt(d, 0);
+    }
+   
+    for (int i = 0; i <= 20; i++){
+        cout << A[i] << " ";
+    }
+	int tmp;
+	cin >> tmp;
+	if (tt(tmp, 0))
+		printf("\n%d is in the tree.", tmp);
+	else
+		printf("\n%d is not in the tree.", tmp);
 	return 0;
 }
 
-void build_tree(int data, int index){
-	if (a[index] == 0)
-		a[index] = data;
-	else{
-		if (data > a[index])
-			build_tree(data, index*2+2);
-		else
-			build_tree(data, index*2+1);
-	}
-	
+void bt(int data, int nodeno){
+    if (A[nodeno] == 0)
+        A[nodeno] = data;
+    else{
+        if (data > A[nodeno])
+        	bt(data, 2 * nodeno + 2);
+        else
+        	bt(data, 2 * nodeno + 1);
+    }
+}
+
+bool tt(int search_target, int nodeno){
+    
+    if (A[nodeno] == 0)
+        return false;
+
+    if (A[nodeno] == search_target)
+        return true;
+
+    if (search_target > A[nodeno])
+        return tt(search_target, 2 * nodeno + 2);
+    else
+        return tt(search_target, 2 * nodeno + 1);
 }
