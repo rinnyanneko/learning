cb2a2122a8d43b528c7ca8c4d42cb3531964d9e9
rinnyanneko <rinnyanneko@outlook.com>
2025-06-25 18:03:23 +0800

    teach

---
+++ Java/teach/Hello World/src/Types.java
@@ -0,0 +1,12 @@
+public class Types {
+    //Primitive types
+    int a = 1;
+    double b = 1.0;
+    boolean c = true;
+    char d = 'a';
+    //Reference types
+    int[] e = {0, 0};
+    String f = "abcde";
+    String i = "1";
+    int t = (int)i;
+}
